3accc3baaebc8c6f19fbb9915d95e9e66da1d349
elbancud <elbancud@gmail.com>
2021-03-19 23:23:44 +0800

    style integrated reg-form initialized

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyG's</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <div class="container">
+                <div class="flex-content">
+
+                    <div class="nav-link">
+                        <div class="logo ">
+                            <div class="dark-font">Bulagtok</div>
+                        </div>
+                    </div>
+                    <div class="nav-link">
+                        <div class="btn dark">
+                            Login
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+    <section>
+
+        <div class="geometric-design">
+            <div class="container">
+                <div class="content-container">
+                    <h1>Let's Start shall we?</h1>
+                </div>
+
+                <div class="circle small  dark"> </div>
+                <div class="circle tiny  dark"> </div>
+                <div class="circle large opacity10 dark"></div>
+                <div class="circle large opacity30 dark"></div>
+                <div class="circle large opacity50 dark"></div>
+                <div class="circle large opacity60 dark"></div>
+
+
+
+            </div>
+
+        </div>
+    </section>
+    <section class="dark large-padding">
+        <div class="container light-color">
+            <h2> Registration</h2>
+
+        </div>
+
+    </section>
+</body>
+
+</html>
